e5f28360a0a550d0a4e506928e718c564ebcbd9d
bvuongx <bvuong@gmail.com>
2013-07-17 11:32:01 -0700

    Associate users to tickets when tickets are created

--- spec/integration/editing_tickets_spec.rb
+++ spec/integration/editing_tickets_spec.rb
@@ -1,22 +1,28 @@
 require 'spec_helper'
 
-feature "Editing tickets" do |variable|
+feature "Editing tickets" do
   let!(:project) { Factory(:project) }
-  let!(:ticket) { Factory(:ticket, :project => project) }
+  let!(:user) { Factory(:confirmed_user) }
+  let!(:ticket) do
+    ticket = Factory(:ticket, :project => project)
+    ticket.update_attribute(:user, user)
+    ticket
+  end
 
   before do
+    sign_in_as!(user)
     visit '/'
     click_link project.name
     click_link ticket.title
     click_link "Edit Ticket"
-end
+  end
 
-scenario "Updating a ticket" do
-  fill_in "Title", :with => "Make it really shiney!"
-  click_button "Update Ticket"
-  page.should have_content "Ticket has been updated."
-  within("#ticket h2") do
-      page.should have_content("Make it really shiney!")
+  scenario "Updating a ticket" do
+    fill_in "Title", :with => "Make it really shiny!"
+    click_button "Update Ticket"
+    page.should have_content "Ticket has been updated."
+    within("#ticket h2") do
+      page.should have_content("Make it really shiny!")
     end
     page.should_not have_content ticket.title
   end
